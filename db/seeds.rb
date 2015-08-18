30b7f57e31e652cd9812dd53ada6eaaa7172091c
Dallie Hill <lenore776@gmail.com>
2015-08-18 17:03:42 -0400

    Modals working, Attachments working, Postgres server

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 BOONDOCK_SAINTS_GIFS = Giphy.search('boondock saints', {limit: 50})
 
 counter = 0
-15.times do |n|
+10.times do |n|
     password = Faker::Internet.password(8)
     user = User.create!(
     email: Faker::Internet.email,
@@ -10,8 +10,8 @@ counter = 0
 
 
     5.times do
-      urls = BOONDOCK_SAINTS_GIFS
-      g = Gif.create!(image_url: url)
+      url = BOONDOCK_SAINTS_GIFS[counter].original_image.url.to_s
+      gif = Gif.create!(url: url)
       user.gifs << gif
       counter = counter + 1
     end
